c00b24f09943c2fbfed68f20626245d61148eb3b
Orelba <88292816+Orelba@users.noreply.github.com>
2021-09-12 21:59:00 +0300

    html mobile optimization

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask_talisman import Talisman  # Added
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_admin import Admin
+from flask_mobility import Mobility
 from .config import Config
 
 ''' Extensions '''
@@ -14,10 +15,27 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+mobility = Mobility()
 
 mail = Mail()
 
-admin = Admin()
+# talisman = Talisman()  # Added
+# csp = {
+#     'default-src': [
+#         '\'self\'',
+#         '\'unsafe-inline\'',
+#         'stackpath.bootstrapcdn.com',
+#         'code.jquery.com',
+#         'cdnjs.cloudflare.com',
+#         'cdn.jsdelivr.net'
+#     ],
+#     'script-src': ["'sha384-(JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM)'",
+#                         'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js',
+#                         'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+#                         'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'
+#                    ],
+#     'style-src': ["'self'", 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css']
+# }
 
 
 def create_app(config_class=Config):
@@ -29,6 +47,8 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    mobility.init_app(app)
+    # talisman.init_app(app, content_security_policy=csp)  # Added
 
     from .users.routes import users
     from .posts.routes import posts
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from flaskblog import create_app  # Imports from __init__.py file
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=False)  # True to enable debug mode
+    app.run(debug=True)  # True to enable debug mode
