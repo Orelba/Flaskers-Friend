5434ed9ea18268a99e7a67ce3e5a4c3ebec328cc
Orel <88292816+Orelba@users.noreply.github.com>
2021-12-26 23:05:10 +0200

    Update utils.py

--- flaskblog/admin/utils.py
+++ flaskblog/admin/utils.py
@@ -45,25 +45,3 @@ def save_add_user_picture(form_picture):
     i.save(picture_path)
 
     return picture_fn
-
-
-# def save_user_picture(user=None, form_picture):
-#     random_hex = secrets.token_hex(8)
-#     _, f_ext = os.path.splitext(form_picture.filename)
-#     picture_fn = random_hex + f_ext
-#     picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
-#     try:
-#         old_picture = user.image_file  # Self added
-#     except Exception:
-#         pass
-#
-#     output_size = (250, 250)
-#     i = Image.open(form_picture)
-#     i.thumbnail(output_size)
-#     i.save(picture_path)
-#     try:
-#         if old_picture != 'default.jpg':
-#             os.remove(f'{current_app.root_path}/static/profile_pics/{old_picture}')
-#     except Exception:
-#         pass
-#     return picture_fn
